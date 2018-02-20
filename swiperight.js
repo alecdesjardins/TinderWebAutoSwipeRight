41df633837a3bddca90c6a8626342fb757f05c6a
alecdesjardins <alec.desjardins@hotmail.com>
2018-02-19 18:28:28 -0600

    Update swiperight.js

--- swiperight.js
+++ swiperight.js
@@ -1,5 +1,5 @@
   a = setInterval(
     function(){
           var element = document.getElementsByClassName("recsGamepad__button--like");
-          elem[0].click()
+          element[0].click()
           },1000) //1000 is 1 second, this can be changed i.e 2000 = 2 seconds
