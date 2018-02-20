4f9b17b488f3e8d08eb5b856360811077bb82ec6
alecdesjardins <alec.desjardins@hotmail.com>
2018-02-19 18:52:00 -0600

    Update swiperight.js

--- swiperight.js
+++ swiperight.js
@@ -3,3 +3,6 @@ a = setInterval(
     function(){
           element[0].click()
           },1000) //1000 is 1 second, this can be changed i.e 2000 = 2 seconds
+
+//to stop the auto swiper, type the following console
+clearInterval(a)
